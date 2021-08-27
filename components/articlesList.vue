83399e3c826e75450cb8db8bc5428e4722a21fc8
RICHARD-Quentin <56677909+RICHARD-Quentin@users.noreply.github.com>
2021-08-27 19:07:18 +0200

    changement couleur boutton

--- components/articlesList.vue
+++ components/articlesList.vue
@@ -49,7 +49,7 @@
 
     <v-card-actions v-show="articles.length > 0">
       <v-row no-gutters justify="center">
-        <v-btn color="#fffbd2"> Passer commande </v-btn>
+        <v-btn color="button-custom-color"> Passer commande </v-btn>
       </v-row>
     </v-card-actions>
   </v-card>
