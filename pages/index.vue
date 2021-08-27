70c1e9390a13ca7ec38710be4d0b2de1d2e3af22
EmmanuelBIGEON <43699856+EmmanuelBIGEON@users.noreply.github.com>
2021-08-27 09:25:23 +0200

    Parallax et Carrousel

--- pages/index.vue
+++ pages/index.vue
@@ -1,79 +1,109 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
+  <v-container>
+    <v-row justify="center" align="center">
+      <AccParallax />
+    </v-row>
+    <v-row>
+      <v-col cols="12" md="7" >
+        <v-card>
+          <v-card-title>
+            <v-carousel
+              cycle
+              height="400px"
             >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+              <v-carousel-item>
+                <v-img
+                  src="/woman.jpg"
+                height="400px"
+                >
+                <v-card-title class="carrousel_title"> Achète mon parpaing. </v-card-title>
+                </v-img>
+              </v-carousel-item>
+
+              <v-carousel-item>
+                <v-img
+                  src="/pink.jpg"
+                  height="400px"
+                >
+                <v-card-title class="carrousel_title"> Je mange des frites des fois le soir </v-card-title>
+                </v-img>
+              </v-carousel-item>
+            </v-carousel>
+
+          </v-card-title>
+        </v-card>
+      </v-col>
+      
+      <v-col cols="12" md="5" >
+        <v-card>
+          <v-card-title> Nos adresses </v-card-title>
+          <v-row>
+            <v-col cols="12" md="2" >
+            <img
+              src="location.svg"
+              alt="un triangle aux trois côtés égaux"
+              height="50px"
+              width="50px" />
+            </v-col>
+            <v-col cols="12" md="9" 
+              justify="center"
+              align="left"
+              class="px-0"
+
+              >
+              <v-card-text class="adresses">
+                  Toulouse
+              </v-card-text>
+            </v-col>
+
+          </v-row>
+
+          <v-row height="40px" no-gutters>
+            <v-col cols="12" md="2" >
+            <img
+              src="location.svg"
+              alt="un triangle aux trois côtés égaux"
+              height="50px"
+              width="50px" />
+            </v-col>
+            <v-col cols="12" md="9" 
+              justify="center"
+              align="left"
+              class="px-0"
+
+              >
+              <v-card-text class="adresses">
+                  Paris
+              </v-card-text>
+            </v-col>
+
+          </v-row>
+
+          <v-row height="30px" no-gutters>
+            <v-col cols="12" md="2" >
+            <img
+              src="location.svg"
+              alt="un triangle aux trois côtés égaux"
+              height="50px"
+              width="50px" />
+            </v-col>
+            <v-col cols="12" md="9" 
+              justify="center"
+              align="left"
+              class="px-0"
+
+              >
+              <v-card-text class="adresses">
+                  Nancy
+              </v-card-text>
+            </v-col>
+          </v-row>
+        </v-card>
+
+      </v-col>
+    </v-row>
+  </v-container>
+  
 </template>
 
 <script>
@@ -87,3 +117,20 @@ export default {
   }
 }
 </script>
+
+<style>
+  .fade-enter-active, .fade-leave-active 
+{
+  transition: opacity .5s;
+}
+
+.carrousel_title
+{
+  position: absolute;
+  bottom: 40px;
+}
+
+.adresses {
+    font-size: 1.2em;
+}
+</style>
