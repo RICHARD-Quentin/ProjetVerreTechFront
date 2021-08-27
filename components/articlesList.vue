984b5f22c43d42d473feedb1c405f8bc6db1be77
RICHARD-Quentin <56677909+RICHARD-Quentin@users.noreply.github.com>
2021-08-27 17:32:46 +0200

    modif articleList

--- components/articlesList.vue
+++ components/articlesList.vue
@@ -1,26 +1,27 @@
 <template>
   <v-card
     class="mx-auto"
-    max-width="300"
     tile
     min-width="250px"
+    max-height="75vh"
   >
-    <v-list>
+    <v-list v-show="articles.length > 0">
       <v-list-item-group>
-        <v-list-item v-for="(article, i) in getArticlesList" :key="i" class="px-0">
+        <v-list-item v-for="(article, i) in articles" :key="i" class="px-0">
           <v-row no-gutters align="center">
             <v-col cols="4">
-              <v-list-item-icon :src="article.image"/>
+              <v-img max-width="80px" max-height="80px" :src="'/' + article.img "/>
             </v-col>
 
             <v-col cols="6">
               <v-list-item-title v-text="article.title"/>
-              <v-list-item-subtitle v-text="article.price + '€'"/>
+              <v-list-item-subtitle v-text="'Quantité: ' + article.quantity"/>
+              <v-list-item-subtitle v-text="'Prix: ' + article.price*article.quantity + '€'"/>
             </v-col>
 
             <v-col cols="1">
               <v-list-item-action>
-                <v-btn @click="this.suppArticle({i})" text small icon>
+                <v-btn @click="suppArticle({i})" text small icon>
                   <v-icon>mdi-delete</v-icon>
                 </v-btn>
               </v-list-item-action>
@@ -29,15 +30,45 @@
         </v-list-item>
       </v-list-item-group>
     </v-list>
+
+    <v-card-text v-show="articles.length > 0">
+      <v-row>
+        Nombre d'articles : {{ getNumberOfArticles }}
+      </v-row>
+
+      <v-row>
+        Prix total : {{ getTotalPrice }}€
+      </v-row>
+    </v-card-text>
+
+
+    <v-card-text v-show="articles.length === 0">
+      Aucun article dans le pannier
+    </v-card-text>
+
+
+    <v-card-actions v-show="articles.length > 0">
+      <v-row no-gutters justify="center">
+        <v-btn color="#fffbd2"> Passer commande </v-btn>
+      </v-row>
+    </v-card-actions>
   </v-card>
 
 </template>
 
 <script>
-import {mapGetters, mapMutations} from "vuex";
+import {mapGetters} from "vuex";
 
 export default {
   name: "articlesList",
+
+  data() {
+    return {
+      articles: [],
+      totalPrice : 0,
+      numberOfArticles: 0
+    }
+  },
 
   methods: {
     suppArticle(articleIndex){
@@ -45,8 +76,33 @@ export default {
     }
   },
 
+  watch: {
+    'getArticlesList':{
+      handler(val) {
+        this.articles = val
+      },
+      deep: true
+    },
+    'getTotalPrice':{
+      handler(val) {
+        this.totalPrice = val
+      },
+      deep: true
+    },
+    'getNumberOfArticles':{
+      handler(val) {
+        this.numberOfArticles = val
+      },
+      deep: true
+    }
+  },
+
   computed: {
-    ...mapGetters('cart', ['getArticlesList'])
+    ...mapGetters('cart', ['getArticlesList', 'getTotalPrice', 'getNumberOfArticles'])
+  },
+
+  mounted() {
+
   }
 }
 </script>
--- store/cart.js
+++ store/cart.js
@@ -12,8 +12,18 @@ export const mutations = {
     state.articleCount --
   },
 
-  addArticle(state, {article}) {
-    state.articles.push(article)
+  addArticle(state, {article, quantity}) {
+
+    let oldArticleIndex = state.articles.findIndex(elem => elem.id === article.id)
+
+    if (oldArticleIndex === -1){
+      let newArticle = Object.assign(article, {quantity: parseInt(quantity)})
+      state.articles.push(newArticle)
+    } else {
+      let newQuantity = state.articles[oldArticleIndex].quantity + parseInt(quantity)
+      state.articles.push({...state.articles[oldArticleIndex], 'quantity': newQuantity})
+      state.articles.splice(oldArticleIndex, 1)
+    }
   },
 
   suppArticle(state, {articleIndex}) {
@@ -23,16 +33,18 @@ export const mutations = {
 
 export const getters = {
   getNumberOfArticles(state) {
-    return state.articles.length
+    return state.articles.reduce((quantity, article) =>
+      quantity + article.quantity, 0
+    )
   },
 
   getArticlesList(state) {
-    return state.articles
+    return [...state.articles].reverse()
   },
 
   getTotalPrice(state) {
-    return state.articles.reduce((total, article) => {
-      total += article.price
-    })
+    return state.articles.reduce((total, article) =>
+      total + article.price*article.quantity, 0
+    )
   }
 }
