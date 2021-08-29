7f75e427f7f94a4b6b28ef4fbfbac45e5e1ba05c
EmmanuelBIGEON <43699856+EmmanuelBIGEON@users.noreply.github.com>
2021-08-29 12:42:39 +0200

    ebauche

--- pages/about.vue
+++ pages/about.vue
@@ -1,8 +1,21 @@
 <template>
-  <div>
-      <p> test </p>
-  </div>
-
+  <v-card>
+      <p> hihi </p>
+      <v-divider class="ma-4"></v-divider>
+    <v-expansion-panels>
+        <v-expansion-panel
+        v-for="(item,i) in 5"
+        :key="i"
+        >
+        <v-expansion-panel-header>
+            Item
+        </v-expansion-panel-header>
+        <v-expansion-panel-content>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </v-expansion-panel-content>
+        </v-expansion-panel>
+    </v-expansion-panels>
+  </v-card>
 </template>
 
 <script>
