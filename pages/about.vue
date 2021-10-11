e68b96d0d6bf173bb30be600f4d4742a3e622ad0
EmmanuelBIGEON <43699856+EmmanuelBIGEON@users.noreply.github.com>
2021-10-11 14:46:36 +0200

    Cover + FAQ

--- pages/about.vue
+++ pages/about.vue
@@ -1,17 +1,30 @@
 <template>
   <v-card>
-      <p> hihi </p>
+    <v-card-title class="justify-center text-h2" >A propos</v-card-title>
+
+    <v-img src="about_cover.jpg" 
+      max-height="400"
+      ></v-img>
+    <v-card-text>
+
+      <p>
+        Nous mangeons des produits français.
+      </p>
+    </v-card-text>
+
       <v-divider class="ma-4"></v-divider>
     <v-expansion-panels>
         <v-expansion-panel
-        v-for="(item,i) in 5"
+        v-for="(item,i) in faq"
         :key="i"
         >
         <v-expansion-panel-header>
-            Item
+            {{item.title}}
         </v-expansion-panel-header>
         <v-expansion-panel-content>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          <v-card color="grey lighten-4" class="pa-10">
+            {{item.content}}
+          </v-card>
         </v-expansion-panel-content>
         </v-expansion-panel>
     </v-expansion-panels>
@@ -22,6 +35,33 @@
 
 export default {
   name: "about",
+
+  data() {
+    return {
+      faq: [
+        {
+          title: "Qui sommes-nous ?",
+          content: "Nous sommes VerreTech, une entreprise spécialisée dans le verre et plus précisement [..]"
+        },
+        {
+          title: "Qu'est ce que le Click & Collect ?",
+          content: "Le Click and Collect est une solution ECommerce permettant de réserver un article en magasin."
+        },
+        {
+          title: "Comment puis-je demander des informations supplémentaires ?",
+          content: "Vous pouvez nous contacter via [..]"
+        },
+        {
+          title: "Dans quel délai puis-je retirer mes produits ?",
+          content: "Vous devez allez chercher votre produit dans le magasin sélectionné dans les 2 heures suivant la réservation. Sinon votre commande retournera en stock et vous serez remboursé."
+        },
+        {
+          title: "Vous souhaitez nous découvir ?",
+          content: "Faites le."
+        }
+      ]
+    }
+  }
 }
 </script>
 
