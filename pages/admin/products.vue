a0c3d296b71814b909de48de3656128851e686b5
Emmanuel BIGEON <43699856+EmmanuelBIGEON@users.noreply.github.com>
2021-10-12 10:19:41 +0200

    IndexAdmin + templateActualite

---
+++ pages/admin/products.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-card>
+        azeaze
+    </v-card>
+</template>
+
+<script>
+
+export default {
+    name: 'admin_products',
+    layout: 'adminlayout',
+    data() {
+        return {
+            message: 'Hello Vue!'
+        }
+    }
+}
+</script>
