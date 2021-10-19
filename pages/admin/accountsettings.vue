c2498e06e391ae93c28e18c4a6f91c95fc34dfcf
EmmanuelBIGEON <43699856+EmmanuelBIGEON@users.noreply.github.com>
2021-10-19 19:25:48 +0200

    Création des pages essentielles

---
+++ pages/admin/accountsettings.vue
@@ -0,0 +1,46 @@
+<template>  
+    <v-row>
+        <v-col cols="3">
+            
+            <admin-menu :menu_item_selected="menu_item_selected"></admin-menu>
+        </v-col>
+        <v-col cols="9">
+            <v-card id="main_content">
+                <v-card-title class="text-center mx-auto d-block"> Paramètre du compte </v-card-title>
+
+            </v-card>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+
+export default {
+    name: 'AccountSettings',
+    layout: 'adminlayout',
+    data() {
+        return {
+            menu_item_selected: 'account_settings',
+        }
+    },
+
+    methods: {
+        displayMessage(text)
+        {
+            console.log(text);
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+
+
+#main_content
+{
+    min-height: 400px;
+    /* background-color: yellow; */
+}
+
+</style>
---
+++ pages/admin/savesbackups.vue
@@ -0,0 +1,46 @@
+<template>  
+    <v-row>
+        <v-col cols="3">
+            
+            <admin-menu :menu_item_selected="menu_item_selected"></admin-menu>
+        </v-col>
+        <v-col cols="9">
+            <v-card id="main_content">
+                <v-card-title class="text-center mx-auto d-block"> Sauvegardes et backups </v-card-title>
+
+            </v-card>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+
+export default {
+    name: 'SavesBackups',
+    layout: 'adminlayout',
+    data() {
+        return {
+            menu_item_selected: 'saves_backups',
+        }
+    },
+
+    methods: {
+        displayMessage(text)
+        {
+            console.log(text);
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+
+
+#main_content
+{
+    min-height: 400px;
+    /* background-color: yellow; */
+}
+
+</style>
