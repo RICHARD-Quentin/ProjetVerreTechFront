34e342faa56bf87847f9260b580c9598a4dec688
Quentin <richard.quentin88@gmail.com>
2021-12-23 20:13:29 +0100

    fixs

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,13 +53,13 @@ export default {
       }
     }
   },
-  
+
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
     '@nuxtjs/auth-next',
-    'vuetify-dialog/nuxt'
+    'vuetify-dialog/nuxt',
     'nuxt-leaflet'
   ],
 
