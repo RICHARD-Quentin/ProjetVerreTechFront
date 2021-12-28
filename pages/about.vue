c038810c8ad51b9e971db0557881446104c51100
EmmanuelBIGEON <43699856+EmmanuelBIGEON@users.noreply.github.com>
2021-12-28 18:01:08 +0100

    Mise à jour des informations et interface de la page A propos

--- pages/about.vue
+++ pages/about.vue
@@ -1,16 +1,10 @@
 <template>
   <v-card>
-    <v-card-title class="justify-center text-h2" >A propos</v-card-title>
+    <v-card-title class="justify-center mb-0 pb-0" >Pouvons-nous répondre à votre question ?</v-card-title>
 
     <v-img src="about_cover.jpg" 
-      max-height="400"
+      max-height="100px" class="ma-5"
       ></v-img>
-    <v-card-text>
-
-      <p>
-        Nous mangeons des produits français.
-      </p>
-    </v-card-text>
 
       <v-divider class="ma-4"></v-divider>
     <v-expansion-panels>
@@ -41,24 +35,23 @@ export default {
       faq: [
         {
           title: "Qui sommes-nous ?",
-          content: "Nous sommes VerreTech, une entreprise spécialisée dans le verre et plus précisement [..]"
+          content: "Le Groupe Verre-Tech est un groupe 100% familial créé en 1955 par M. Emile Dupont, qui reste," +
+"encore aujourd’hui le PDG de l’entreprise. " +
+"Depuis son origine Verre-tech est spécialisée et leader dans la fabrication et la distribution en " +
+"magasins de produits en verre techniques et décoratifs."
         },
         {
           title: "Qu'est ce que le Click & Collect ?",
-          content: "Le Click and Collect est une solution ECommerce permettant de réserver un article en magasin."
+          content: "Le click and collect, retrait en magasin, cliqué-retiré, service cliquer et ramasser, clique et rapplique, ramassage en magasin ou drive piéton, est un service permettant aux consommateurs de commander en ligne pour ensuite retirer leur article dans un magasin de proximité."
         },
         {
           title: "Comment puis-je demander des informations supplémentaires ?",
-          content: "Vous pouvez nous contacter via [..]"
+          content: "Vous pouvez nous envoyer un mail ou nous appeler directement, nous sommes à votre écoute !"
         },
         {
           title: "Dans quel délai puis-je retirer mes produits ?",
           content: "Vous devez allez chercher votre produit dans le magasin sélectionné dans les 2 heures suivant la réservation. Sinon votre commande retournera en stock et vous serez remboursé."
         },
-        {
-          title: "Vous souhaitez nous découvir ?",
-          content: "Faites le."
-        }
       ]
     }
   }
